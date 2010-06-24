0d8d4273a31feaf24f8c6a0280481d3b587675ed
Elemag <g.mitsov@gmail.com>
2010-06-24 13:00:51 +0300

    Added cyanogen_hero.mk.

---
+++ added_files/cyanogen_hero.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This is the top-level configuration for a US-configured CyanogenMod build
+
+$(call inherit-product, vendor/cyanogen/products/cyanogen.mk)
+
+USE_CAMERA_STUB := false
+
+PRODUCT_NAME := cyanogen_hero
+PRODUCT_BRAND := htc
+PRODUCT_DEVICE := hero
+PRODUCT_MODEL := Hero
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=EPE54B BUILD_DISPLAY_ID=EPE54B BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.1-update1/ERE27/24178:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.1-update1 ERE27 24178 release-keys"
+
+PRODUCT_PACKAGES += \
+    Stk
+
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-5-$(shell date +%m%d%Y)-NIGHTLY-DS
+else
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-5.0.8-DS
+endif
+
+include frameworks/base/data/sounds/AudioPackage4.mk 
+include vendor/htc/hero/device_hero.mk
+
+PRODUCT_COPY_FILES +=  \
+    vendor/htc/hero/media/bootanimation.zip:system/media/bootanimation.zip
+
